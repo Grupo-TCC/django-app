69bc55bfa86d74ec7ba68f565bd5b8b234cc9b22
Emerson Okorie <emersonok52@gmail.com>
2025-10-26 12:54:44 -0300

    settings.py update

--- setup/settings.py
+++ setup/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "85.31.231.8", "innovasusbr.com", "www.innovasusbr.com"]
 
 
 # Application definition
